0c7a59808195c719e8103e1b416ad33b8268a1b5
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-25 16:56:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Write your code in this file!
 
+function getUKTime() {
+let time =  new Date().getHours() + 5
+  return time
+}
+ 
+getUKTime()
 
+function discountTicket(x) {
+  let discount = x * 0.2
+  let price = x - discount
+  return price
+}
+
+function tweetReply(x) {
+  let fanName = x
+  return "Hey " + fanName + ", thanks so much for the support! I really appreciate it!"
+}
